eaf6be54a2b84852df0a918b113ba898e12501b6
DejfL <dawidszymczak92@gmail.com>
2022-09-28 14:10:44 +0200

    tt

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,60 @@
 import React from "react";
 import "./App.css";
-import SwitchButton from "./SwitchButton";
+
+const data = [
+  {
+    id: 1,
+    title: "Wiadomosc nr 1",
+    body: "Zawartosc numer 1",
+  },
+  {
+    id: 2,
+    title: "Wiadomosc nr 2",
+    body: "Zawartosc numer 2",
+  },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomosc nr ${index}`,
+    body: `Zawartosc numer ${index}`,
+  });
+}, 5000);
 
 class App extends React.Component {
   state = {
-    time: 0,
-    acive: false,
+    comments: [...data],
   };
 
-  handleClick = () => {
-    if (this.state.active) {
-      clearInterval(this.idInterval);
-      this.setState({ time: 0 });
-    } else {
-      this.idInterval = setInterval(() => this.addSecond(), 1000);
-    }
-
-    this.setState({ active: !this.state.acive });
-  };
-
-  addSecond() {
-    this.setState({ time: this.state.time + 1 });
+  componentDidMount() {
+    this.idInterval = setInterval(this.getData, 5000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.idInterval);
+  }
+
+  getData = () => {
+    if (this.state.comments.length !== data.length) {
+      this.setState({ comments: [...data] });
+    }
+  };
+
   render() {
-    return (
-      <div className="card">
-        <div>
-          <p>{this.state.time}</p>
-          <SwitchButton onClick={this.handleClick} active={this.state.active} />
-        </div>
+    const comments = this.state.comments.map((comment) => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
       </div>
+    ));
+
+    return (
+      <>
+        <div>Działa</div>
+        {comments.reverse()}
+      </>
     );
   }
 }
